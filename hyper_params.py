9f3334d5c68856113a3c7f765ce6fe3a3bbed50e
hwangso595 <57157860+hwangso595@users.noreply.github.com>
2022-01-22 16:10:50 -0500

    added README

--- hyper_params.py
+++ hyper_params.py
@@ -29,8 +29,8 @@ NOISE_X = 0.75
 DIR_ALPHA = 1
 
 # SETTINGS
-CHESS_GAMES_PATH = 'C:/Users/sungh/Desktop/projects/reinforcement learning/alphazero/chess_games'
-STORE_DATA_BASE = 'D:/chess_numpy'
-MODEL = 'model.h5'
-BEST_MODEL = 'best_model.h5'
+CHESS_GAMES_PATH = './games' # contains pgn files for the network to train on
+STORE_DATA_BASE = './datas' # path to store the numpy datasets
+MODEL = 'model.h5' # can be ignored since we are using checkpoints
+BEST_MODEL = 'best_model.h5' # can be ignored since we are using checkpoints
 STORE_MODEL_BASE = 'models'
